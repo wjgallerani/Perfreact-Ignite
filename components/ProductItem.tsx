8636119d0915f7d88279b43dc13a578d0300ff2a
William Jose Gallerani <wjgallerani@saomanoel.com.br>
2021-10-22 00:06:55 -0300

    Atualização

--- components/ProductItem.tsx
+++ components/ProductItem.tsx
@@ -1,15 +1,41 @@
+import { memo, useState } from 'react';
+import { AddProductToWishlistProps } from './AddProductToWishlist';
+import dynamic from 'next/dynamic';
+
+const AddProductToWishlist = dynamic<AddProductToWishlistProps>(() => {
+    return import('./AddProductToWishlist').then(mod => mod.AddProductToWishlist)
+}, {
+    loading: () => <span>...Carregando</span>
+});
+
 interface ProductItemProps {
     product: {
         id: number;
         price: number;
+        priceFornatted: string;
         title: string;
-    }
+    },
+    onAddToWishlist: (id: number) => void;
 }
 
-export function ProductItem({ product }: ProductItemProps) {
+function ProductItemComponent({ product, onAddToWishlist }: ProductItemProps) {
+    const [isAddingToWishlist, setIsAddingToWishlist] = useState(false);
+
     return (
         <div>
-            {product.title} - <strong>{product.price}</strong>
+            {product.title} - <strong>{product.priceFornatted}</strong>
+            <button onClick={() => setIsAddingToWishlist(true)}>Adicionar aos Favoritos</button>
+
+            {isAddingToWishlist &&
+                (<AddProductToWishlist
+                    onAddToWishlist={() => onAddToWishlist(product.id)}
+                    onRequestClose={() => setIsAddingToWishlist(false)}
+                />)
+            }
         </div>
     );
 }
+
+export const ProductItem = memo(ProductItemComponent, (prevProps, nextProps) => {
+    return Object.is(prevProps.product, nextProps.product);
+});
--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -1,21 +1,54 @@
+import { List, ListRowRenderer } from 'react-virtualized';
 import { ProductItem } from "./ProductItem";
 
 interface SearchResultsProps {
+    totalPrice: number;
     results: Array<{
         id: number;
         price: number;
+        priceFornatted: string;
         title: string;
-    }>
+    }>,
+    onAddToWishlist: (id: number) => void;
 }
 
-export function SearchResults({ results }: SearchResultsProps) {
+export function SearchResults({ totalPrice, results, onAddToWishlist }: SearchResultsProps) {
+
+    const rowaRenderer: ListRowRenderer = ({ index, key, style }) => {
+        return (
+            <div key={key} style={style}>
+                <ProductItem
+                    product={results[index]}
+                    onAddToWishlist={onAddToWishlist}
+                />
+            </div>
+
+
+        );
+    };
+
     return (
         <div>
-            {results.map(product => {
+            <h2>{totalPrice}</h2>
+
+            <List
+                height={300}
+                rowHeight={30}
+                width={900}
+                overscanRowCount={5}
+                rowCount={results.length}
+                rowRenderer={rowaRenderer}
+            />
+
+            {/* {results.map(product => {
                 return (
-                    <ProductItem product={product} />
+                    <ProductItem
+                        key={product.id}
+                        product={product}
+                        onAddToWishlist={onAddToWishlist}
+                    />
                 );
-            })}
+            })} */}
         </div>
     );
 }
